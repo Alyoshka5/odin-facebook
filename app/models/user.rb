ada517b71b7e9320d351aee6de624d186531e523
Alyoshka <alyoshkamyshko@gmail.com>
2022-10-01 23:08:56 -0700

    Add first and last name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
