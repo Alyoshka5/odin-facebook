7c854cbe0960ebf5f941f382fe981401c5d47e1d
Alyoshka <alyoshkamyshko@gmail.com>
2022-10-02 18:29:40 -0700

    Create Users controller and seed users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_names = ["Eberhard", "Aele", "Caratācos", "Rositsa", "Henrietta", "Georgie", "Stefanie", "Daviti", "Reece", "Saewine"]
+last_names = ["Lucero", "Russell", "Walton", "Hardy", "Santos", "Whitehead", "Cisneros", "Barnett", "Davis", "Arellano"]
+
+10.times do |i|
+    User.create(first_name: first_names[i], last_name: last_names[i], email: "#{first_names[i].downcase}#{last_names[i].downcase}@gmail.com", password: first_names[i])
+end
