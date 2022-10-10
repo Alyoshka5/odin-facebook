8b4c8d221ad3e6952ac31378af5295ff682280f7
Alyoshka <alyoshkamyshko@gmail.com>
2022-10-10 15:01:46 -0700

    Display post comments in post comments index view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
     def index
         @post = Post.find(params[:post_id])
         @comment = Comment.new
+        @comments = @post.comments
     end
     
     def create
