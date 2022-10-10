9ad2d0e03490434b97c46981ce193226f78cfc18
Alyoshka <alyoshkamyshko@gmail.com>
2022-10-09 20:22:26 -0700

    Create Post form to allow user to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
     def index
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params())
+
+        if @post.save
+            redirect_to root_path
+        else
+            redirect_to root_path
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:content)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'friends/index'
   get 'friends/destroy'
   get 'friends/show'
+  
+  devise_for :users
+
   resources :friend_requests
   resources :friendships
-  devise_for :users
   resources :users
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
