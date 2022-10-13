a1ef2f93265ac5ca10957c948acbd72e1e80afc5
Alyoshka <alyoshkamyshko@gmail.com>
2022-10-12 20:37:02 -0700

    Move profile image from user to profile model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_one_attached :profile_image
   has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, through: :friend_requests, source: :friend
   has_many :friendships, dependent: :destroy
