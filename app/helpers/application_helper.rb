708f712b7903de4b375a44894bb56907e73f54d0
Alyoshka <alyoshkamyshko@gmail.com>
2022-10-27 20:57:21 -0700

    Style users index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-    def user_profile_image(user, size=40)
+    def user_profile_image(user, size=40, borderRadius="50%")
         profile = user.profile
         if profile.profile_image.attached?
-            image_tag profile.profile_image.variant(resize_to_fill: [size, nil]), style: "border-radius: 50%"
+            image_tag profile.profile_image.variant(resize_to_fill: [size, nil]), style: "border-radius: #{borderRadius}"
         else
             image_tag "https://protechlighting.com/wp-content/uploads/sites/101/2019/12/profile-placeholder-square.png", width: size, height: size, style: "border-radius: 50%"
         end
